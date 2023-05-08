b0a055a38b672db2b1c87d3ac39b60a23f219934
Bitsu <bitsua.mamo@gmail.com>
2023-05-08 18:03:50 -0500

    Finished Week two Day one

---
+++ W2D1/BankAccount.js
@@ -0,0 +1,19 @@
+class BankAccount{
+    accoutName;
+    balance;
+
+    static accountInfoList = [];
+    
+    constructor(accoutName, balance){
+        this.accoutName = accoutName;
+        this.balance = balance;
+    }
+
+    toString(){
+        return `Account Name: ${this.accoutName} Balance: ${this.balance}`;
+    }
+
+    getAccountName(){
+        return this.accoutName;
+    }
+}
