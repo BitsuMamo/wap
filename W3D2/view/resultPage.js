5a349e5d52aa5039a452bd81d860c164d1d69748
Bitsu <bitsua.mamo@gmail.com>
2023-05-17 01:09:52 -0500

    Added Css

--- W3D2/view/resultPage.js
+++ W3D2/view/resultPage.js
@@ -2,7 +2,12 @@ const urlParams = new URLSearchParams(window.location.search);
 const answer = urlParams.get('answer');
 
 const result = document.getElementById("result");
+const goBackBtn = document.getElementById("go-back-btn");
 
 console.log(answer)
 
 result.innerHTML = answer;
+
+goBackBtn.addEventListener('click', () => {
+    window.location.href = "./index.html"
+});
